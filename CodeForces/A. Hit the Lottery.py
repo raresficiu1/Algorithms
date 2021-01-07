b1e8adcd14eabd1975aaa9d6825b6c4b1bdf80c0
Rares <raresficiu1@gmail.com>
2021-01-07 13:46:44 +0000

    BINGO

---
+++ CodeForces/A. Hit the Lottery.py
@@ -0,0 +1,28 @@
+#https://codeforces.com/problemset/problem/996/A
+
+
+bank= int(input())
+bills=0
+while bank:
+
+    if(bank>=100):
+        aux=bank//100
+        bank-=aux*100
+        bills+=aux
+    elif (bank >= 20):
+        aux = bank //20
+        bank -= aux *20
+        bills += aux
+    elif (bank >= 10):
+        aux = bank //10
+        bank -= aux *10
+        bills += aux
+    elif (bank >= 5):
+        aux = bank //5
+        bank -= aux *5
+        bills += aux
+    else:
+        bills+=bank
+        bank=0
+
+print(bills)
