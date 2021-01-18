c9d92e3f0877c3fcc616bae93112259c015ae0c5
Rares <raresficiu1@gmail.com>
2021-01-18 13:48:19 +0000

    Insertion Sort

---
+++ Basic/Insertion Sort.py
@@ -0,0 +1,18 @@
+#insertion sort
+
+
+def insertion_sort(list):
+
+    for j in range(1,len(list)):
+        key=list[j]
+
+        i=j-1
+
+        while(i>=0 and list[i]>key):
+            list[i+1] = list[i]
+            i-=1
+        list[i+1]=key
+
+    return list
+
+print(insertion_sort([5 ,2 ,4 ,6 ,1, 3]))
