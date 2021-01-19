1f4244928311108edcdc225436709ed10a3b2e6a
Rares <raresficiu1@gmail.com>
2021-01-19 05:21:32 +0000

    Merge sort

---
+++ Basic/Sort.py
@@ -0,0 +1,50 @@
+#insertion sort
+
+
+def insertion_sort(list):
+
+    for j in range(1,len(list)):
+        key=list[j]
+
+        i=j-1
+
+        while(i>=0 and list[i]>key):
+            list[i+1] = list[i]
+            i-=1
+        list[i+1]=key
+
+    return list
+
+def merge_sort(list):
+    if len(list) > 1:
+        mid = len(list) // 2
+        L = list[:mid]
+        R = list[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                list[k] = L[i]
+                i += 1
+            else:
+                list[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            list[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            list[k] = R[j]
+            j += 1
+            k += 1
+
+        return list
+print(insertion_sort([5 ,2 ,4 ,6 ,1, 3]))
+print(merge_sort([5 ,2 ,4 ,6 ,1, 3]))
