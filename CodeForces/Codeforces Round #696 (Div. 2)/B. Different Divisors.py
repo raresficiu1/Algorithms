6a988900a470ea251399bb1a199ad890f7d6aa8e
Rares <raresficiu1@gmail.com>
2021-01-19 18:02:38 +0000

    codeforces 696 TLE2

--- CodeForces/Codeforces Round #696 (Div. 2)/B. Different Divisors.py
+++ CodeForces/Codeforces Round #696 (Div. 2)/B. Different Divisors.py
@@ -5,43 +5,58 @@
 tests = int(input())
 d=[]
 max=0
-for i in range(tests):
 
+for i in range(tests):
     o = int(input())
     d.append(o)
     if(o>max):
         max=o
 
-
 solFound=False
 start=1
 primes=[]
 
+def primes_sieve1():
+    limitn = 10000
+    primes = dict()
+    for i in range(2, limitn): primes[i] = True
 
-while solFound==False:
+    for i in primes:
+        factors = range(i,limitn,i)
+        for f in factors[1:]:
+            primes[f] = False
+    return [i for i in primes if primes[i]==True]
+
+
+
+
+'''while solFound==False:
     found=False
-    if start > 1:
+    if start >= 1:
         for i in range(2, start):
             if (start % i) == 0:
                 break
         else:
             primes.append(start)
             found=True
-    start+=1
+
 
     if(found==True):
         for i in range(len(primes)):
             for j in range(1,len(primes)):
                 if(primes[j]-primes[i]>=max):
                     solFound=True
+    start += 1'''
 
+primes=primes_sieve1()
 
+found=0
 for i in range(len(d)):
-    found==False
+    found=False
     for j in range(len(primes)):
         if(primes[j]>=1+d[i]):
             for k in range(1, len(primes)):
-                if (primes[k] - primes[j] >= d[i] ):
+                if (primes[k] - primes[j] >= d[i]):
                     print(primes[k]*primes[j])
                     found=True
                     break
